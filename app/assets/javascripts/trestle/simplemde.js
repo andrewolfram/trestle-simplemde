048d039df28d409a0cd477465cd9c37d230b5658
André Wolfram <andre@42ways.de>
2023-04-17 12:34:15 +0200

    add even handlers for simpleMDE refreshes

--- app/assets/javascripts/trestle/simplemde.js
+++ app/assets/javascripts/trestle/simplemde.js
@@ -6,5 +6,10 @@ Trestle.init(function(e, root) {
   $('textarea.simplemde', root).each(function() {
     var options = $.extend({}, { element: this }, Trestle.SimpleMDE);
     var simplemde = new SimpleMDE(options);
+    $(document).on("shown.bs.modal", ".modal", function () {
+            simplemde.codemirror.refresh();
+        }).on('shown.bs.tab', 'a[data-toggle="tab"]', function () {
+            simplemde.codemirror.refresh();
+        })
   });
 });
